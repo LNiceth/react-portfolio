bf1783c3dd099df4193593c6682abce99443f5a0
Lord Niceth Cuevas <lordnicethcuevas@gmail.com>
2022-07-24 14:34:12 +1000

    changing some paths

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
         <Route path="/work" element={<Work />}></Route>
         <Route path="/projects" element={<Jsprojects />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
-        <Route path="*" element={<PageNotFound />}></Route>
+        <Route path="*" element={<Home />}></Route>
       </Routes>
     </div>
   );
